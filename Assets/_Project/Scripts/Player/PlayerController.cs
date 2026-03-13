a5cfec896689b62345c50e2e4dd216c880bffed3
Simone Ruocco <simone.ruocco97@icloud.com>
2026-03-13 19:46:21 +0100

    aggiunto salto e sprint, gestione della gravità e groundcheck

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -1,59 +1,75 @@
 using UnityEngine;
 
+//use gravity off - interpolate - collision su continuous
+
 public class PlayerController : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private GroundCheck groundCheck;
     //[SerializeField] private AnimationManager animator;
 
     [Header("Movement Attributes")]
     [SerializeField] private float speed = 2f;
-    //[SerializeField] private float multipliedSpeed = 2f;
+    [SerializeField] private float speedMultiplier = 2f;
+    private float currentSpeed;
     private Vector3 move;
-    private float horizontal;
-    private float vertical;
 
     [Header("Rotation Attributes")]
     [SerializeField] private float rotationSmoothness = 10f;
     private Quaternion rotTarget;
 
-    //[Header("Jump Attributes")]
-    //[SerializeField] private GroundCheck groundCheck;
-    //[SerializeField] private float jumpHeight = 2f;
+    [Header("Jump Attributes")]
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float fallMultiplier = 2.5f;
+    private float verticalVelocity = 0f;
 
     private void Awake()
     {
         if (rb == null)
             rb = GetComponent<Rigidbody>();
 
-        //if (groundCheck == null)
-        //    groundCheck = GetComponent<GroundCheck>();
+        if (groundCheck == null)
+            groundCheck = GetComponent<GroundCheck>();
 
         //if (animator == null)
         //    animator = GetComponent<AnimationManager>();
-
     }
 
     private void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-
-        move = new Vector3(horizontal, 0, vertical);
-
-        //SprintCheck();
-
-        //if (Input.GetButtonDown("Jump"))
-        //    PerformJump();
+        PlayerInput();
+        JumpCheck();
+        SprintCheck();
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + move * (speed * Time.deltaTime));
+        ApplyGravity();
+        PlayerMovement();
+        PlayerRotation();
 
         //animator.SetJumpState(groundCheck.CheckIsGrounded());
         //animator.JumpAnimation();
+    }
 
+    private void PlayerInput()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        move = new Vector3(horizontal, 0, vertical).normalized;
+    }
+
+    private void PlayerMovement()
+    {
+        Vector3 velocity = move * currentSpeed;
+        velocity.y = verticalVelocity;
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    private void PlayerRotation()
+    {
         if (move != Vector3.zero)
         {
             rotTarget = Quaternion.LookRotation(move);
@@ -61,22 +77,31 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    //private void SprintCheck()
-    //{
-    //    float currentSpeed = initialSpeed;
+    private void SprintCheck()
+    {
+        currentSpeed = speed;
+        if (Input.GetButton("Fire3"))
+            currentSpeed *= speedMultiplier;
+    }
 
-    //    if (Input.GetButton("Fire3"))
-    //        currentSpeed *= multipliedSpeed;
+    private void ApplyGravity()
+    {
+        if (groundCheck.CheckIsGrounded() && verticalVelocity < 0f) 
+            verticalVelocity = -2f; //se e' a terra mantienilo tale con valore minimo X (non si usa -9.81f perche' potrebbe causare compenetrazioni con il terreno)
+        else                        //altrimenti applica la gravita' con fall multiplier se sta cadendo
+        {
+            float appliedGravity = (verticalVelocity < 0f) ? gravity * fallMultiplier : gravity;
+            verticalVelocity += appliedGravity * Time.fixedDeltaTime;
+        }
+    }
 
-    //    speed = currentSpeed;
-    //}
-
-    //private void PerformJump()
-    //{
-    //    if (!groundCheck.CheckIsGrounded())
-    //        return;
-
-    //    rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-    //    animator.TriggerJump();
-    //}
+    private void JumpCheck()
+    {
+        if (Input.GetButtonDown("Jump") && groundCheck.CheckIsGrounded())
+        {
+            verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        //rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        //animator.TriggerJump();
+    }
 }
