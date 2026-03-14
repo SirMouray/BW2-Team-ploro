b8837c1d634293fb306104187daa106c9959ac0b
Jaco <59362156+jacobbe987@users.noreply.github.com>
2026-03-14 15:54:11 +0100

    TestPooling maps

--- Assets/_Project/Scripts/Maps/Maps.cs
+++ Assets/_Project/Scripts/Maps/Maps.cs
@@ -1,10 +1,26 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class Maps : MonoBehaviour
 {
+    [SerializeField] private bool _isDeactive = false;
     private IObjectPool<Maps> _objPool;
     public IObjectPool<Maps> ObjPool { set => _objPool = value; }
+
+    IEnumerator DeactiveRoutine()
+    {
+        //yield return new WaitForSeconds(delay);
+        yield return new WaitUntil(() => _isDeactive);
+        // resettare tutta la mappa
+        _objPool.Release(this);
+        _isDeactive = false;
+    }
+
+    public void Deactive()
+    {
+        StartCoroutine(DeactiveRoutine());
+    }
+
+    
 }
--- Assets/_Project/Scripts/ObjectPooling/ObjectPooling.cs
+++ Assets/_Project/Scripts/ObjectPooling/ObjectPooling.cs
@@ -1,25 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class ObjectPooling : MonoBehaviour
 {
-    [SerializeField] private Maps _objPrefab;
+    [SerializeField] private Maps[] _objPrefab;
     private IObjectPool<Maps> _objPool;
     private bool _collectionCheck;
-    private int _poolCapacity;
-    private int _poolMaxSize;
+    private int _poolCapacity = 5;
+    private int _poolMaxSize = 10;
 
     private void Awake()
     {
-        _objPool = new ObjectPool<Maps>(Create,OnGetFromPool,OnRealeseToPool, OnDestroyPooledObj,_collectionCheck,_poolCapacity,_poolMaxSize);
+        _objPool = new ObjectPool<Maps>(Create, OnGetFromPool, OnRealeseToPool, OnDestroyPooledObj, _collectionCheck, _poolCapacity, _poolMaxSize);
     }
 
     private Maps Create()
     {
-        Maps obj = Instantiate(_objPrefab);
+        Maps obj = Instantiate(_objPrefab[Random.Range(0, _objPrefab.Length)]);
+        obj.gameObject.SetActive(false);
         obj.ObjPool = _objPool;
         return obj;
     }
@@ -37,5 +37,15 @@ public class ObjectPooling : MonoBehaviour
     private void OnDestroyPooledObj(Maps pooledObj)
     {
         Destroy(pooledObj.gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Maps mapsobj = _objPool.Get();
+            mapsobj.transform.SetPositionAndRotation(new Vector3(Random.Range(0, 20), Random.Range(0, 20)), Quaternion.identity);
+            mapsobj.Deactive();
+        }
     }
 }
